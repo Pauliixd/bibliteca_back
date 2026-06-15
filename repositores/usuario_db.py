0bafabbfcbe1ef0a3d03f0b5841e38b37eb70d10
Pauliixd <pauliisidoro1@gmail.com>
2026-06-15 18:32:46 -0300

    primer commit

---
+++ repositores/usuario_db.py
@@ -0,0 +1,4 @@
+from repositores.usuario_db import crear_usuario, obtener_todos_usuario, eliminar_usuario, modificar_usuario, obtener_usuario_dni
+ #FALTA HACER EN REPOSITORES 
+ 
+
---
+++ routes/usuario.py
@@ -0,0 +1,128 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session, select
+from datetime import timedelta
+
+# Importa tus modelos y configuración desde su ubicación real
+from models.usuario import Usuario, UserCreate, UserPublic, UserUpdate, PasswordUpdate
+#from repositores.usuario_db import crear_usuario, obtener_todos_usuario, eliminar_usuario, modificar_usuario, obtener_usuario_dni
+ #FALTA HACER EN REPOSITORES
+from database.database import get_db
+
+#FALTA HACER EL SERVICE
+from services.user_services import (
+    obtener_password_hash,
+    verificar_password,
+    crear_token_acceso,
+   # get_current_user,
+    ACCESS_TOKEN_EXPIRE_MINUTES
+)
+
+router = APIRouter(prefix="/users", tags=["Users"])
+
+# End points publicos (sin autorizacion)
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def registrar_usuario(
+    usuario_in: UserCreate,
+    db: Annotated[Session, Depends(get_db)]
+):
+    #verificar que el username no exista
+    statement_username = select(Usuario).where(Usuario.username == usuario_in.username)
+    if db.exec(statement_username).first():
+        raise HTTPException(status_code=status.HTTP_404_BAD_REQUEST, detail="El nombre de usuario ya esta registrado",
+        )
+    #validar el gmail
+    statement_email = select(Usuario).where(Usuario.email == usuario_in.email)
+    if db.exec(statement_email).first():
+        raise HTTPException(status_code=status.HTTP_404_BAD_REQUEST, detail="El gmail ya esta registrado",
+        )
+    
+    # Encriptar el nuevo usuario
+    nuevo_user = Usuario(
+        username = usuario_in.username,
+        email= usuario_in.email,
+        nombre= usuario_in.nombre,
+        apellido=usuario_in.apellido,
+        password_hashed=obtener_password_hash(usuario_in.password),
+        disabled=False,
+    )
+    db.add(nuevo_user)
+    db.commit()
+    db.refresh(nuevo_user)
+    
+    return {"mensaje" : "usuario registrado"}
+
+@router.post("/token")
+async def login_para_obtener_token(
+    form_data:Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[Session, Depends(get_db)],
+):
+    #Endpoint para autenticarse e intercambiar credenciales por un JWT.
+    statement = select(Usuario).where(Usuario.username == form_data.username)
+    user = db.exec(statement).first()
+    
+    if not user or not verificar_password(form_data.password, user.password_hashed):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuario o contrasenia incorrecta",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = crear_token_acceso(
+        data={"sub": user.username}, expires_delta= access_token_expires
+    )
+    
+    return {"access_token": access_token, "token_type":"bearer"}
+
+    #JWT (Json Web Token) contiene informacion codificada
+    
+    # Dependencias para endpoints protegidos (requieren autenticacion)
+
+#dependencia para obtener el usuario actual a partir del token JWT incluido en la solicitud
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Annotated[Session, Depends(get_db)],
+    ) -> Usuario:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="No se pudieron validar las credenciales",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KAY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    
+# buscar el usuario en la base de datos 
+
+    statement = select(Usuario).where(Usuario.username == username)
+    user = db.exec(statement).first()
+    if user is None:
+        raise credentials_exception
+    if user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario inactivo")
+    
+    return user
+    
+   # Dependencia para verificar roles de usuario en endpoints protegidos 
+class RoleChecker:
+    def __init__(self, allowed_roles: list[str]):
+        #recibe una lista de roles permitidos para acceder a un endpoint
+        self.allowed_roles = allowed_roles
+
+        def __call__(self, current_user: Annotated[Usuario, Depends(get_current_user)]) -> Usuario:
+
+            if current_user.role not in self.allowed_roles:
+                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No tenes los permisos necesarios para realizar esta accion")
+            return current_user
+ 
+
+
+
+        
